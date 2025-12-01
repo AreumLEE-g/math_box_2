445b25d3896f18d840a4e58ac709fcaeffe03c0e
unknown <arjpga@gmail.com>
2025-12-01 09:26:52 +0900

    Add mutiply func

--- calculator.js
+++ calculator.js
@@ -6,3 +6,8 @@ const substract = (a, b) => a - b;
 
 // git init - 로컬 레포지토리 만들기
 // git.add
+
+// type (x: number, y: number) -> number; 리턴값이 넘버라는 뜻.
+const multiply = (a, b) => {
+  return a * b;
+};
